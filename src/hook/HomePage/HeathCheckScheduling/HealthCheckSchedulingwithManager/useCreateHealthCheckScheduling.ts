874eeb76e4acafcd46fe21929e15d85042671c76
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2024-03-18 12:10:10 +0700

    dev:done fix dev-TF-135-Func_CreateHeathCheckScheduling

--- src/hook/HomePage/HeathCheckScheduling/HealthCheckSchedulingwithManager/useCreateHealthCheckScheduling.ts
+++ src/hook/HomePage/HeathCheckScheduling/HealthCheckSchedulingwithManager/useCreateHealthCheckScheduling.ts
@@ -1,86 +1,94 @@
-import  { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-shadow */
+import { useState } from 'react';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../../../type/type';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ApiCreateHealthCheckRemindSchedule } from '../../../../api/useApiCreateHeathCheckRemindScheduling';
+import { Alert } from 'react-native';
 interface CreateHealthCheck {
     reExaminationTime: string;
     reExaminationDate: string;
     reExaminationLocation: string;
     nameHospital: string;
     userNote: string;
-  }
-const useCreateHealthCheck = () =>{
+}
+const useCreateHealthCheck = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [selectedDate, setSelectedDate] = useState('');
     const showDatePicker = () => {
-      setDatePickerVisibility(true);
+        setDatePickerVisibility(true);
     };
     const hideDatePicker = () => {
-      setDatePickerVisibility(false);
+        setDatePickerVisibility(false);
     };
     const handleConfirm = (date) => {
-      hideDatePicker();
-      setSelectedDate(date.toISOString());
+        const currentDate = new Date();
+        const selectedDate = new Date(date);
+        if (selectedDate < currentDate) {
+            Alert.alert('Please select a future date for health check.');
+            return; // Exit function if selected date is in the past
+        }
+        hideDatePicker();
+        setSelectedDate(selectedDate.toISOString());
     };
     const handleCancel = () => {
-      setModalVisible(false);
+        setModalVisible(false);
     };
     const handleOK = () => {
-      setModalVisible(false);
-      navigation.navigate('HomePage');
+        setModalVisible(false);
+        navigation.navigate('HomePage');
     };
     const useGoBack = () => {
-      navigation.goBack();
+        navigation.goBack();
     };
     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
     const mutationCreatHealthCheck = useMutation({
-      mutationFn: async (data: CreateHealthCheck) => {
-        try {
-          const userId = await AsyncStorage.getItem('userId');
-          const requestData = {
-            ...data,
-            userId: userId,
-          };
-          const response = await axios.post(ApiCreateHealthCheckRemindSchedule, requestData);
-          setTimeout(async () => {
-            if (response.status === 200) {
-              const {completed, message} = response.data;
-              console.log('data', data);
-              if (completed) {
-                setModalVisible(true);
-                console.log('Create Health Check successfully.');
-              } else {
-                console.log('Create Heath Check  failed:', message);
-              }
-              // setIsLoading(false);
-            } else {
-              console.log('Response:', response);
-              console.log('Error:', 'Unexpected response status');
+        mutationFn: async (data: CreateHealthCheck) => {
+            try {
+                const userId = await AsyncStorage.getItem('userId');
+                const requestData = {
+                    ...data,
+                    userId: userId,
+                };
+                const response = await axios.post(ApiCreateHealthCheckRemindSchedule, requestData);
+                setTimeout(async () => {
+                    if (response.status === 200) {
+                        const { completed, message } = response.data;
+                        console.log('data', data);
+                        if (completed) {
+                            setModalVisible(true);
+                            console.log('Create Health Check successfully.');
+                        } else {
+                            console.log('Create Heath Check  failed:', message);
+                        }
+                        // setIsLoading(false);
+                    } else {
+                        console.log('Response:', response);
+                        console.log('Error:', 'Unexpected response status');
+                    }
+                }, 2000);
+                clearForm();
+            } catch (error: any) {
+                console.log('Error sending the request:', error.message);
+                if (error.response && error.response.status === 409) {
+                    console.log('User has been successfully registered');
+                } else {
+                    console.log('Unexpected error:', error);
+                }
             }
-          }, 2000);
-          clearForm();
-        } catch (error: any) {
-          console.log('Error sending the request:', error.message);
-          if (error.response && error.response.status === 409) {
-            console.log('User has been successfully registered');
-          } else {
-            console.log('Unexpected error:', error);
-          }
-        }
-      },
+        },
     });
     const clearForm = () => {
-      setModalVisible(true);
-      mutationCreatHealthCheck.reset();
+        setModalVisible(true);
+        mutationCreatHealthCheck.reset();
     };
     return {
         modalVisible,
         setModalVisible,
-        hideDatePicker ,
+        hideDatePicker,
         isDatePickerVisible,
         setDatePickerVisibility,
         selectedDate,
@@ -94,4 +102,4 @@ const useCreateHealthCheck = () =>{
         mutationCreatHealthCheck,
     };
 };
-export  default useCreateHealthCheck;
+export default useCreateHealthCheck;
