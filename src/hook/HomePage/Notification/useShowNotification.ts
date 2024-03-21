467b1160f18eafc76201cdb62af7f56f05dcab97
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2024-03-22 03:29:29 +0700

    dev:done dev-TF-153-ImplementShowNotificationInApp

---
+++ src/hook/HomePage/Notification/useShowNotification.ts
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+interface Medication {
+    medicationName: string;
+    dosage: number | null;
+    _id: string;
+}
+interface TreatmentNotification {
+    _id: string;
+    treatmentTime: string;
+    medications: Medication[];
+    noteTreatment: string | null;
+    username: string;
+    deviceToken: string;
+    userId: string;
+    __v: number;
+}
+interface HealthNotification {
+    _id: string;
+    reExaminationDate: string;
+    reExaminationTime: string;
+    reExaminationLocation: string;
+    nameHospital: string;
+    userNote: string;
+    username: string;
+    deviceToken: string;
+    userId: string;
+    __v: number;
+}
+interface DataNotifications {
+    treatmentNotifications: TreatmentNotification[];
+    healthNotifications: HealthNotification[];
+}
+const useShowNotification = () =>{
+    const shouldRefetch = useRef<boolean>(false);
+    const [selectedCategory, setSelectedCategory] = useState<'treatment' | 'health'>('treatment'); // Thêm state để theo dõi loại thông báo được chọn
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { data, isError, refetch } = useQuery<DataNotifications[]>({
+        queryKey: ['healthcheckReminders'],
+        queryFn: async () => {
+            try {
+                const userId = await AsyncStorage.getItem('userId');
+                console.log(userId);
+                const response = await axios.get<DataNotifications[]>(
+                    `http://www.whales-fhn.dns-dynamic.net:8000/Notification/getNotifications/${userId}`,
+                    {
+                        headers: {
+                            Accept: 'application/json',
+                        },
+                    },
+                );
+                if (response.status === 200) {
+                    const outData = response.data.dataNotifications;
+                    console.log('====================================');
+                    console.log(response.data.dataNotifications);
+                    console.log('====================================');
+                    shouldRefetch.current = true;
+                    return outData;
+                } else if (response.status === 404) {
+                    throw new Error('Data not found');
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+                throw new Error('Failed to fetch data');
+            }
+        },
+    });
+    useEffect(() => {
+        setIsLoading(true); // Reset isLoading to true on each render
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+    return {shouldRefetch,selectedCategory,setSelectedCategory, isLoading, setIsLoading,data,isError, refetch,};};
+export default  useShowNotification;
---
+++ src/screen/HomePage/Notification/Notificationswhenusingtheapp/RenderTreatment.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'; // Import TouchableOpacity
+import rendernewnotification from '../../../../styles/HomePage/Notification/Notificationswhenusingtheapp/RenderNewNotification';
+import RenderTreatmentNotifications from './RenderTreatmentItem';
+import RenderHealthNotifications from './RenderHealthCheckItem';
+import useShowNotification from '../../../../hook/HomePage/Notification/useShowNotification';
+const renderNewNotification = () => {
+    const {shouldRefetch,selectedCategory, setSelectedCategory, isLoading, setIsLoading, data,isError,refetch} = useShowNotification();
+    const renderTreatmentNotifications = () => (
+        <RenderTreatmentNotifications data={data?.treatmentNotifications || []} />
+    );
+    const renderHealthNotifications = () => (
+        <RenderHealthNotifications data={data?.healthNotifications || []} />
+    );
+    return (
+        <View style={rendernewnotification.container1}>
+            <View style={rendernewnotification.tabContainer}>
+                <TouchableOpacity style={[rendernewnotification.tabButton, selectedCategory === 'treatment' && rendernewnotification.activeTabButton]} >
+                   <Text style={[rendernewnotification.tabText, selectedCategory === 'treatment' && rendernewnotification.tabText1]}>Treatment</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[rendernewnotification.tabButton, selectedCategory === 'health' && rendernewnotification.activeTabButton]} onPress={() => setSelectedCategory('health')}>
+                     <Text style={[rendernewnotification.tabText, selectedCategory === 'health' && rendernewnotification.tabText1]}>HealthCheck</Text>
+                </TouchableOpacity>
+            </View>
+            {isLoading ? (
+                <ActivityIndicator size="large" color="#87CEFA" />
+            ) : (
+                <>
+                    {selectedCategory === 'treatment' ? renderTreatmentNotifications() : renderHealthNotifications()}
+                </>
+            )}
+        </View>
+    );
+};
+export default renderNewNotification;
